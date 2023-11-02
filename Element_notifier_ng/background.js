5894dd6a2607e620c20766c20346fc0ad8429fd1
ujoehero <roberts@aphios.ro>
2023-11-02 21:41:21 +0200

    simple optimization i hope

--- Element_notifier_ng/background.js
+++ Element_notifier_ng/background.js
@@ -1,8 +1,6 @@
 // Get current Tab
 async function getCurrentTab() {
-  let queryOptions = { active: true, lastFocusedWindow: true };
-  // `tab` will either be a `tabs.Tab` instance or `undefined`.
-  let [tab] = await chrome.tabs.query(queryOptions);
+  const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
   return tab;
 }
 
@@ -13,16 +11,19 @@ var workingTabs = [];
 var timeValue = 60;
 
 async function initialize() {
-  watchingUrlsStorage =
-    (await chrome.storage.local.get("watchingUrlsStorage"))
-      .watchingUrlsStorage || [];
-  watchingAttributesStorage =
-    (await chrome.storage.local.get("watchingAttributesStorage"))
-      .watchingAttributesStorage || [];
-  timeValue = (await chrome.storage.local.get("timeValue")).timeValue || 60;
+  const defaultValues = await chrome.storage.local.get({
+    watchingUrlsStorage: [],
+    watchingAttributesStorage: [],
+    timeValue: 60,
+  });
+
+  watchingUrlsStorage = defaultValues.watchingUrlsStorage;
+  watchingAttributesStorage = defaultValues.watchingAttributesStorage;
+  timeValue = defaultValues.timeValue;
 }
+
 initialize();
-// Clean context on install/update
+
 
 chrome.runtime.onInstalled.addListener(async () => {
   for (const cs of chrome.runtime.getManifest().content_scripts) {
@@ -35,153 +36,279 @@ chrome.runtime.onInstalled.addListener(async () => {
   }
 });
 
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.url) {
+//     const index = watchingUrlsStorage.indexOf(request.url);
+//     if (index === -1) {
+//       watchingUrlsStorage.push(request.url);
+//       chrome.storage.local.set({ watchingUrlsStorage });
+//       sendResponse({
+//         status: "Done",
+//         message: "[+]",
+//         currentValue: watchingUrlsStorage,
+//       });
+//     } else {
+//       watchingUrlsStorage.splice(index, 1);
+//       chrome.storage.local.set({ watchingUrlsStorage });
+//       sendResponse({
+//         status: "Done",
+//         message: "[-]",
+//         currentValue: watchingUrlsStorage,
+//       });
+//     }
+//   } else if (request.attrKey) {
+//     let attrKey = request.attrKey;
+//     let attrValue = request.attrValue;
+
+//     const index = watchingAttributesStorage.findIndex((object) => {
+//       return object.attrKey === attrKey && object.attrValue === attrValue;
+//     });
+//     if (index === -1) {
+//       // Create a new object
+//       let newObj = { attrKey: attrKey, attrValue: attrValue };
+//       watchingAttributesStorage.push(newObj);
+//     } else {
+//       // Remove the object if it already exists
+//       watchingAttributesStorage.splice(index, 1);
+//     }
+//     chrome.storage.local.set({ watchingAttributesStorage });
+//     sendResponse({
+//       status: "Done",
+//       message: index === -1 ? "[+]" : "[-]",
+//       currentValue: watchingAttributesStorage,
+//     });
+//   } else if (request.action === "getSelectedElement") {
+//     sendResponse(globalSelectedElement);
+//   } else if (request.type === "trigger") {
+//     let tabstatus = sender.tab.active;
+//     if (watchingUrlsStorage.includes(request.validateTab)) {
+//       sendResponse({
+//         status: "Done",
+//         validateTab: true,
+//         attrs: watchingAttributesStorage,
+//         tabstatus: tabstatus,
+//       });
+//     } else {
+//       sendResponse({ status: "Done", validateTab: false });
+//     }
+//   } else if (request.timevalue) {
+//     let temptime = request.timevalue;
+//     if (temptime < 60) {
+//       temptime = 60;
+//     } else if (temptime > 900) {
+//       temptime = 900;
+//     }
+
+//     if (typeof temptime === "number") {
+//       timeValue = temptime;
+//       chrome.storage.local.set({ timeValue });
+//       sendResponse({
+//         status: "Done",
+//         currentValue: timeValue,
+//       });
+//     } else {
+//       timeValue = 60;
+//       chrome.storage.local.set({ timeValue });
+//       sendResponse({
+//         status: "Done",
+//         currentValue: timeValue,
+//       });
+//     }
+//   } else if (request.type === "notification") {
+//     let msgtxt = request.msg;
+//     let notid = `${request.notid}_${sender.tab.id}_${sender.tab.windowId}`;
+//     let opts = {
+//       type: "basic",
+//       iconUrl: "icon.png",
+//       title: "Element found!",
+//       message: msgtxt,
+//       contextMessage: request.contexturl,
+//       priority: 2,
+//     };
+//     chrome.notifications.create(notid, opts);
+//   } else if (request.type === "reload") {
+//     chrome.tabs.reload(request.tabid);
+//   } else if (request.type === "getHelp") {
+//     chrome.tabs.create({
+//       url: "help.html"
+//     });
+//   } else if (request.type === "selectbadge") {
+//     createBadge(request.selector);
+//   }
+// });
+
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.url) {
-    const index = watchingUrlsStorage.indexOf(request.url);
-    if (index === -1) {
-      watchingUrlsStorage.push(request.url);
-      chrome.storage.local.set({ watchingUrlsStorage });
-      sendResponse({
-        status: "Done",
-        message: "[+]",
-        currentValue: watchingUrlsStorage,
-      });
-    } else {
-      watchingUrlsStorage.splice(index, 1);
-      chrome.storage.local.set({ watchingUrlsStorage });
-      sendResponse({
-        status: "Done",
-        message: "[-]",
-        currentValue: watchingUrlsStorage,
-      });
-    }
+    handleUrlRequest(request, sendResponse);
   } else if (request.attrKey) {
-    let attrKey = request.attrKey;
-    let attrValue = request.attrValue;
-
-    const index = watchingAttributesStorage.findIndex((object) => {
-      return object.attrKey === attrKey && object.attrValue === attrValue;
-    });
-    if (index === -1) {
-      // Create a new object
-      let newObj = { attrKey: attrKey, attrValue: attrValue };
-      watchingAttributesStorage.push(newObj);
-    } else {
-      // Remove the object if it already exists
-      watchingAttributesStorage.splice(index, 1);
-    }
-    chrome.storage.local.set({ watchingAttributesStorage });
-    sendResponse({
-      status: "Done",
-      message: index === -1 ? "[+]" : "[-]",
-      currentValue: watchingAttributesStorage,
-    });
+    handleAttributeRequest(request, sendResponse);
   } else if (request.action === "getSelectedElement") {
     sendResponse(globalSelectedElement);
   } else if (request.type === "trigger") {
-    let tabstatus = sender.tab.active;
-    if (watchingUrlsStorage.includes(request.validateTab)) {
-      sendResponse({
-        status: "Done",
-        validateTab: true,
-        attrs: watchingAttributesStorage,
-        tabstatus: tabstatus,
-      });
-    } else {
-      sendResponse({ status: "Done", validateTab: false });
-    }
+    handleTriggerRequest(request, sendResponse, sender);
   } else if (request.timevalue) {
-    let temptime = request.timevalue;
-    if (temptime < 60) {
-      temptime = 60;
-    } else if (temptime > 900) {
-      temptime = 900;
-    }
-
-    if (typeof temptime === "number") {
-      timeValue = temptime;
-      chrome.storage.local.set({ timeValue });
-      sendResponse({
-        status: "Done",
-        currentValue: timeValue,
-      });
-    } else {
-      timeValue = 60;
-      chrome.storage.local.set({ timeValue });
-      sendResponse({
-        status: "Done",
-        currentValue: timeValue,
-      });
-    }
+    handleTimeValueRequest(request, sendResponse);
   } else if (request.type === "notification") {
-    let msgtxt = request.msg;
-    let notid = `${request.notid}_${sender.tab.id}_${sender.tab.windowId}`;
-    let opts = {
-      type: "basic",
-      iconUrl: "icon.png",
-      title: "Element found!",
-      message: msgtxt,
-      contextMessage: request.contexturl,
-      priority: 2,
-    };
-    chrome.notifications.create(notid, opts);
+    handleNotificationRequest(request, sender);
   } else if (request.type === "reload") {
-    chrome.tabs.reload(request.tabid);
+    handleReloadRequest(request);
   } else if (request.type === "getHelp") {
-    chrome.tabs.create({
-      url: "help.html"
-    });
+    openHelpPage();
   } else if (request.type === "selectbadge") {
     createBadge(request.selector);
   }
 });
 
-// Do stuff on tabs
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // skip urls like "chrome://" to avoid extension error
-  if (tab.url?.startsWith("chrome://")) {
-    return undefined;
-  } else if (changeInfo.url && watchingUrlsStorage.includes(changeInfo.url)) {
-    // URL has changed to a watched URL
-  }
-});
+// Define separate functions to handle different types of requests
 
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: contentScriptFunc,
-    args: ["action"],
-  });
-});
-
-function contentScriptFunc(name) {
-  alert(`"${name}" executed`);
-}
-function createBadge(status = "") {
-  if (status) {
-    chrome.action.setBadgeText({ text: "SEL" });
-    chrome.action.setBadgeBackgroundColor({ color: "#6c0909" });
+function handleUrlRequest(request, sendResponse) {
+  const index = watchingUrlsStorage.indexOf(request.url);
+  if (index === -1) {
+    watchingUrlsStorage.push(request.url);
+    chrome.storage.local.set({ watchingUrlsStorage });
+    sendResponse({
+      status: "Done",
+      message: "[+]",
+      currentValue: watchingUrlsStorage,
+    });
   } else {
-    chrome.action.setBadgeText({});
+    watchingUrlsStorage.splice(index, 1);
+    chrome.storage.local.set({ watchingUrlsStorage });
+    sendResponse({
+      status: "Done",
+      message: "[-]",
+      currentValue: watchingUrlsStorage,
+    });
   }
+}
+
+function handleAttributeRequest(request, sendResponse) {
+  const attrKey = request.attrKey;
+  const attrValue = request.attrValue;
+  const index = watchingAttributesStorage.findIndex((object) => {
+    return object.attrKey === attrKey && object.attrValue === attrValue;
+  });
+  if (index === -1) {
+    const newObj = { attrKey, attrValue };
+    watchingAttributesStorage.push(newObj);
+  } else {
+    watchingAttributesStorage.splice(index, 1);
+  }
+  chrome.storage.local.set({ watchingAttributesStorage });
+  sendResponse({
+    status: "Done",
+    message: index === -1 ? "[+]" : "[-]",
+    currentValue: watchingAttributesStorage,
+  });
+}
+
+function handleTriggerRequest(request, sendResponse, sender) {
+  const tabstatus = sender.tab.active;
+  if (watchingUrlsStorage.includes(request.validateTab)) {
+    sendResponse({
+      status: "Done",
+      validateTab: true,
+      attrs: watchingAttributesStorage,
+      tabstatus,
+    });
+  } else {
+    sendResponse({ status: "Done", validateTab: false });
+  }
+}
+
+function handleTimeValueRequest(request, sendResponse) {
+  let temptime = request.timevalue;
+  temptime = Math.min(Math.max(temptime, 60), 900); // Clamp between 60 and 900
+  timeValue = typeof temptime === "number" ? temptime : 60;
+  chrome.storage.local.set({ timeValue });
+  sendResponse({
+    status: "Done",
+    currentValue: timeValue,
+  });
+}
+
+function handleNotificationRequest(request, sender) {
+  const msgtxt = request.msg;
+  const notid = `${request.notid}_${sender.tab.id}_${sender.tab.windowId}`;
+  const opts = {
+    type: "basic",
+    iconUrl: "icon.png",
+    title: "Element found!",
+    message: msgtxt,
+    contextMessage: request.contexturl,
+    priority: 2,
+  };
+  chrome.notifications.create(notid, opts);
+}
+
+function handleReloadRequest(request) {
+  chrome.tabs.reload(request.tabid);
+}
+
+function openHelpPage() {
+  chrome.tabs.create({
+    url: "help.html"
+  });
+}
+
+// // Do stuff on tabs
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//   // skip urls like "chrome://" to avoid extension error
+//   if (tab.url?.startsWith("chrome://")) {
+//     return undefined;
+//   } else if (changeInfo.url && watchingUrlsStorage.includes(changeInfo.url)) {
+//     // URL has changed to a watched URL
+//   }
+// });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (isChromeInternalUrl(tab.url)) {
+    return;
+  }
+
+  if (changeInfo.url && isWatchedUrl(changeInfo.url)) {
+    // URL has changed to a watched URL
+    handleWatchedUrlChange(tabId, changeInfo.url);
+  }
+});
+
+function isChromeInternalUrl(url) {
+  return url && url.startsWith("chrome://");
+}
+
+function isWatchedUrl(url) {
+  return watchingUrlsStorage.includes(url);
+}
+
+function handleWatchedUrlChange(tabId, changedUrl) {
+  // Perform actions when a watched URL changes in a tab
+  // You can implement specific logic here
+}
+
+
+function createBadge(status = "", customText = "SEL", customColor = "#6c0909") {
+  const badgeText = status ? customText : "";
+  const badgeColor = status ? customColor : [0, 0, 0, 0];
+
+  chrome.action.setBadgeText({ text: badgeText });
+  chrome.action.setBadgeBackgroundColor({ color: badgeColor });
 }
 
 // This callback WILL NOT be called for "_execute_action"
-chrome.commands.onCommand.addListener((command, tab) => {
+chrome.commands.onCommand.addListener(async (command, tab) => {
   if (tab.url?.startsWith("chrome://")) {
-    return undefined;
+    return;
   }
-  (async () => {
-    const response = await chrome.tabs.sendMessage(tab.id, { type: "select" });
-    // createBadge(response.selector);
-  })();
+
+  const response = await chrome.tabs.sendMessage(tab.id, { type: "select" });
 });
 
-chrome.notifications.onClicked.addListener(function (notifId) {
-  notifdata = notifId.split("_");
-  tabId = parseInt(notifdata[1]);
-  windowId = parseInt(notifdata[2]);
-  chrome.tabs.get(tabId, function (tab) {
-    chrome.tabs.highlight({ tabs: tab.index, windowId }, function () {});
+chrome.notifications.onClicked.addListener((notifId) => {
+  const [_, tabId, windowId] = notifId.split("_").map(Number);
+
+  chrome.tabs.get(tabId, (tab) => {
+    chrome.tabs.highlight({ tabs: tab.index, windowId }, () => { });
   });
 });
